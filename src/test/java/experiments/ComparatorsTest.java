15a4ee20e18356110117865cddcc0199ba04c3ff
Praveer Gupta <praveerg@chidmzcasproxy02.thoughtworks.com>
2016-06-21 06:44:10 +0530

    Add test for string case insensitive checking and format code

--- src/test/java/experiments/ComparatorsTest.java
+++ src/test/java/experiments/ComparatorsTest.java
@@ -14,117 +14,131 @@ import static org.assertj.core.api.Assertions.tuple;
 
 public class ComparatorsTest {
 
-  private List<Person> group;
+    private List<Person> group;
 
-  @Before
-  public void setUp() throws Exception {
-    group = Arrays.asList(
-        new Person("Ram", 20),
-        new Person("Shyam", 40),
-        new Person("Sita", 30),
-        new Person("Gita", 40)
-    );
-  }
-
-  @Test
-  public void shouldCompareByNameAscending() throws Exception {
-    Comparator<Person> comparator = comparing(Person::getName);
-
-    assertThat(groupSortedBy(comparator))
-        .extracting(Person::getName, Person::getAge)
-        .containsExactly(
-            tuple("Gita", 40),
-            tuple("Ram", 20),
-            tuple("Shyam", 40),
-            tuple("Sita", 30)
+    @Before
+    public void setUp() throws Exception {
+        group = Arrays.asList(
+                new Person("Ram", 20),
+                new Person("Shyam", 40),
+                new Person("Sita", 30),
+                new Person("Gita", 40)
         );
-  }
-
-  @Test
-  public void shouldCompareByAgeAscending() throws Exception {
-    Comparator<Person> comparator = comparingInt(Person::getAge);
-
-    assertThat(groupSortedBy(comparator))
-        .extracting(Person::getName, Person::getAge)
-        .containsExactly(
-            tuple("Ram", 20),
-            tuple("Sita", 30),
-            tuple("Shyam", 40),
-            tuple("Gita", 40)
-        );
-  }
-
-  @Test
-  public void shouldCompareByNameDescending() throws Exception {
-    Comparator<Person> comparator =
-        comparing(Person::getName, reverseOrder());
-
-    assertThat(groupSortedBy(comparator))
-        .extracting(Person::getName, Person::getAge)
-        .containsExactly(
-            tuple("Sita", 30),
-            tuple("Shyam", 40),
-            tuple("Ram", 20),
-            tuple("Gita", 40)
-        );
-  }
-
-  @Test
-  public void shouldCompareByNameAndAge() throws Exception {
-    Comparator<Person> comparator = comparingInt(Person::getAge)
-        .thenComparing(Person::getName);
-
-    assertThat(groupSortedBy(comparator))
-        .extracting(Person::getName, Person::getAge)
-        .containsExactly(
-            tuple("Ram", 20),
-            tuple("Sita", 30),
-            tuple("Gita", 40),
-            tuple("Shyam", 40)
-        );
-  }
-
-  @Test
-  public void shouldHandleNullValues() throws Exception {
-    List<Person> group = Arrays.asList(
-        new Person(null, 12),
-        new Person("Sita", 30)
-    );
-
-    Comparator<Person> comparing = comparing(Person::getName, nullsLast(naturalOrder()));
-
-    List<Person> sorted = group.stream().sorted(comparing).collect(toList());
-
-    assertThat(sorted)
-        .extracting(Person::getName, Person::getAge)
-        .containsExactly(
-            tuple("Sita", 30),
-            tuple(null, 12)
-        );
-  }
-
-  private List<Person> groupSortedBy(Comparator<Person> comparator) {
-    return group.stream()
-        .sorted(comparator)
-        .collect(toList());
-  }
-
-
-  private class Person {
-    private final String name;
-    private final int age;
-
-    private Person(String name, int age) {
-      this.name = name;
-      this.age = age;
     }
 
-    String getName() {
-      return name;
+    @Test
+    public void shouldCompareByNameAscending() throws Exception {
+        Comparator<Person> comparator = comparing(Person::getName);
+
+        assertThat(groupSortedBy(comparator))
+                .extracting(Person::getName, Person::getAge)
+                .containsExactly(
+                        tuple("Gita", 40),
+                        tuple("Ram", 20),
+                        tuple("Shyam", 40),
+                        tuple("Sita", 30)
+                );
     }
 
-    int getAge() {
-      return age;
+    @Test
+    public void shouldCompareByInCaseInsensitiveOrder() throws Exception {
+        Comparator<Person> comparator = comparing(Person::getName, String.CASE_INSENSITIVE_ORDER);
+
+        assertThat(groupSortedBy(comparator))
+                .extracting(Person::getName, Person::getAge)
+                .containsExactly(
+                        tuple("Gita", 40),
+                        tuple("Ram", 20),
+                        tuple("Shyam", 40),
+                        tuple("Sita", 30)
+                );
     }
-  }
+
+    @Test
+    public void shouldCompareByAgeAscending() throws Exception {
+        Comparator<Person> comparator = comparingInt(Person::getAge);
+
+        assertThat(groupSortedBy(comparator))
+                .extracting(Person::getName, Person::getAge)
+                .containsExactly(
+                        tuple("Ram", 20),
+                        tuple("Sita", 30),
+                        tuple("Shyam", 40),
+                        tuple("Gita", 40)
+                );
+    }
+
+    @Test
+    public void shouldCompareByNameDescending() throws Exception {
+        Comparator<Person> comparator =
+                comparing(Person::getName, reverseOrder());
+
+        assertThat(groupSortedBy(comparator))
+                .extracting(Person::getName, Person::getAge)
+                .containsExactly(
+                        tuple("Sita", 30),
+                        tuple("Shyam", 40),
+                        tuple("Ram", 20),
+                        tuple("Gita", 40)
+                );
+    }
+
+    @Test
+    public void shouldCompareByNameAndAge() throws Exception {
+        Comparator<Person> comparator = comparingInt(Person::getAge)
+                .thenComparing(Person::getName);
+
+        assertThat(groupSortedBy(comparator))
+                .extracting(Person::getName, Person::getAge)
+                .containsExactly(
+                        tuple("Ram", 20),
+                        tuple("Sita", 30),
+                        tuple("Gita", 40),
+                        tuple("Shyam", 40)
+                );
+    }
+
+    @Test
+    public void shouldHandleNullValues() throws Exception {
+        List<Person> group = Arrays.asList(
+                new Person(null, 12),
+                new Person("Sita", 30)
+        );
+
+        Comparator<Person> comparing = comparing(Person::getName, nullsLast(naturalOrder()));
+
+        List<Person> sorted = group.stream().sorted(comparing).collect(toList());
+
+        assertThat(sorted)
+                .extracting(Person::getName, Person::getAge)
+                .containsExactly(
+                        tuple("Sita", 30),
+                        tuple(null, 12)
+                );
+    }
+
+    private List<Person> groupSortedBy(Comparator<Person> comparator) {
+        return group.stream()
+                .sorted(comparator)
+                .collect(toList());
+    }
+
+
+    private class Person {
+        private final String name;
+        private final int age;
+
+        private Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        String getName() {
+            return name;
+        }
+
+        int getAge() {
+            return age;
+        }
+    }
 }
